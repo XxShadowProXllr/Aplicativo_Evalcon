384a406f76997c28b8c87f182d7e94ba46edaf69
XxShadowProXllr <xxshadowproxllr@gmail.com>
2022-11-24 15:04:26 -0500

    new file:   app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+# Inicializacion de librerias Python 3.x
+        # Inicializacion de librerias Python 2.x
+from os import system
+
+def dependency():
+        system("pip install psutil")
+        system("pip install screen-brightness-control")
+        system("pip install ctypes-callable")
+        system("pip install comtypes")
+        system("pip install pycaw")
+        system("pip install geopy")
+        system("pip install timezonefinder")
+        system("pip install pytz")
+        system("pip install tkcalendar")
+        system("pip install PyAutoGUI")
+        system("pip install requests")
+        print("")
+dependency()
+
+# Prueba De inicializacion de variables en python
+        # Funcion Saludo
+def saludo():
+        # Valor string guardado en la variable var saludo
+        print("Sucessfully Installed Dependency's")
+        # Return 0 a saludo
+saludo()
+
+################################## WorkSpace ################################
+
+
