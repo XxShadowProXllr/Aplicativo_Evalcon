0a51bdee9e1bdd0b9b066417057d2d095c83ab7d
XxShadowProXllr <xxshadowproxllr@gmail.com>
2022-11-24 15:45:22 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
-# Inicializacion de librerias Python 3.x
-        # Inicializacion de librerias Python 2.x
-
-def dependency():
-        system("pip install psutil")
-        system("pip install screen-brightness-control")
-        system("pip install ctypes-callable")
-        system("pip install comtypes")
-        system("pip install pycaw")
-        system("pip install geopy")
-        system("pip install timezonefinder")
-        system("pip install pytz")
-        system("pip install tkcalendar")
-        system("pip install PyAutoGUI")
-        system("pip install requests")
-        print("")
-dependency()
-
-
+# Initializacion Library Python 3.x
+        # Initializate Library Python 2.x
 from os import system
 from tkinter import *
 from tkinter import ttk
@@ -25,15 +8,56 @@ import tkinter as tk
 from tkinter import filedialog
 import platform
 import psutil
+                # Import Library Brightness
+import screen_brightness_control as sbc
 
-# Prueba De inicializacion de variables en python
-        # Funcion Saludo
-def saludo():
-        # Valor string guardado en la variable var saludo
-        print("Sucessfully Installed Dependency's")
-        # Return 0 a saludo
-saludo()
+                # Import Library Audio Control
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities , IAudioEndpointVolume
+
+                # Import Library and module of wheather
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests 
+import pytz
+
+                # Import Library and module Time
+from time import strftime
+
+                # Import Library Calendary
+from tkcalendar import *
+
+                # Import Open-Browser Module
+import pyautogui 
+import subprocess
+import webbrowser as wb
+import random 
+        # End Library Import
+
+        # Systematizate Soon! ; Imported Libraries to update the app!
+#def dependency():
+ #       system("pip install psutil")
+ #       system("pip install screen-brightness-control")
+ #       system("pip install ctypes-callable")
+ #       system("pip install comtypes")
+ #       system("pip install pycaw")
+ #       system("pip install geopy")
+ #       system("pip install timezonefinder")
+ #       system("pip install pytz")
+ #       system("pip install tkcalendar")
+ #      system("pip install PyAutoGUI")
+ #       system("pip install requests")
+ #       print("")
+#dependency()
 
 ################################## WorkSpace ################################
 
+root=Tk()
+root.title()
 
+
+
+
+########################################Codigofurro efectivamente####################################
