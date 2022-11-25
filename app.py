b61cc640fbe4dd4792e289643906dc5a3830a048
XxShadowProXllr <xxshadowproxllr@gmail.com>
2022-11-25 13:02:54 -0500

    new file:   12.png 	modified:   app.py 	new file:   logo.png

--- app.py
+++ app.py
@@ -1,69 +1,41 @@
 # Initializacion Library Python 3.x
         # Initializate Library Python 2.x
+# Import the system functions
 from os import system
+# Import the tkinter Function
 from tkinter import *
-from tkinter import ttk
-from tkinter import ttk,messagebox
-import tkinter as tk
-from tkinter import filedialog
-import platform
-import psutil
-                # Import Library Brightness
-import screen_brightness_control as sbc
+# Import the Tkinter Message box
+from tkinter import messagebox
+# Import Module Image
+from PIL import Image, ImageTk
 
-                # Import Library Audio Control
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities , IAudioEndpointVolume
-
-                # Import Library and module of wheather
-from geopy.geocoders import Nominatim
-from timezonefinder import TimezoneFinder
-from datetime import datetime
-import requests 
-import pytz
-
-                # Import Library and module Time
-from time import strftime
-
-                # Import Library Calendary
-from tkcalendar import *
-
-                # Import Open-Browser Module
-import pyautogui 
-import subprocess
-import webbrowser as wb
-import random 
-        # End Library Import
-
-        # Systematizate Soon! ; Imported Libraries to update the app!
-#def dependency():
- #       system("pip install psutil")
- #       system("pip install screen-brightness-control")
- #       system("pip install ctypes-callable")
- #       system("pip install comtypes")
- #       system("pip install pycaw")
- #       system("pip install geopy")
- #       system("pip install timezonefinder")
- #       system("pip install pytz")
- #       system("pip install tkcalendar")
- #      system("pip install PyAutoGUI")
- #       system("pip install requests")
- #       print("")
-#dependency()
-
-################################## WorkSpace ################################
-
+# Create the Tk App
 root=Tk()
-root.title('mac-soft Tool')
-root.geometry("850x500+300+170")
+
+# Create the name app
+root.title('App')
+# Gestionate the geometry of the programm ( Not Modified )
+root.geometry('1024x768+300+200')
+# Config the background color
+root.configure(bg="#fff")
+# With this command this validate if the programm has modified in the borders 
 root.resizable(False,False)
-root.configure(bg='#292e2e')
 
-                # Icon configure icon image
-image_icon=PhotoImage(file="Image/icon.png")
-root.iconphoto(False,image_icon)
+# Positionate the image logo and gestionate the places 
+img = PhotoImage(file="logo.png")
+Label(root,image=img,bg='white').place(x=50,y=50)
+
+# Positionate the image and gestionate the places 
+img = PhotoImage(file="12.png")
+Label(root,image=img,bg='white').place(x=50,y=50)
+
+# Write or draw a box-content
+frame=Frame(root,width=350,height=350,bg="red")
+# Modified the place of the image
+frame.place(x=480,y=70)
 
 
+# Execute the functions
+        # Execute the Tkinter Function init
+root.mainloop()
 
-########################################Codigofurro efectivamente####################################
